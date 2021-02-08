7f6a0c7da3e23b9a2438e8cd5c4ad37c29781ba4
Harlez <62451203+Harlez@users.noreply.github.com>
2021-02-08 16:55:20 -0300

    Add files via upload

---
+++ src/components/HeroSection.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import '../App.css'
+import { Button } from './Button'
+import './HeroSection.css'
+
+function HeroSection() {
+    return (
+        <div className= 'hero-container'>
+            <video src="/videos/video-1.mp4" autoPlay loop muted  />
+            <h1>SindiPostos</h1>
+            <p>Sindicato do Comércio varejista de derivados de petróleo do Estado do Ceará</p>
+            <div className="hero-btns">
+                <Button className='btns' 
+                buttonStyle='btn--outline'
+                buttonSize='btn-large'
+                > CONHEÇA-NOS 
+                </Button>
+
+                <Button className='btns' 
+                buttonStyle='btn--primary'
+                buttonSize='btn-large'
+                > Entre em contato <i class="fas fa-gas-pump"></i>
+                </Button>
+
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
