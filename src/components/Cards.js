eb8ea739bef0dde60f25e927f223c7eeaed8529d
Harlez <62451203+Harlez@users.noreply.github.com>
2021-02-15 16:44:24 -0300

    Add files via upload

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -9,34 +9,31 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem 
-                            src="images/img-JuazeiroNO.jpg"
-                            text="Av. Ailton Gomes, 4131 – Sala 207 – Lagoa Seca
-                            – Cep: 63020-000,
-                            Fone: (88) 3571.2208
-                            subsederegiaodocariri@sindipostos-ce.com.br"
-                            label='Juazeiro do Norte'
+                    <CardItem 
+                            src="images/img-Fortal.jpg"
+                            text="Avenida Engenheiro Santana Junior, 
+                            3000, sala: 605 CEP: 60192-200 
+                            Cel:(85) 9 9137-7964 Telefone: (85) 3244.1147 
+                            Email:sindipostos@sindipostos-ce.com.br"
+                            label='Fortaleza'
                             path='/services'
                         />
-
                         <CardItem 
                             src="images/img-Sobral.jpg"
                             text="Rua Dr. Arimatéia Monte e Silva, 1125 – Junco,
                             Cep: 62030-230
                             Fone: (88) 3614.5912
-                            subsederegiaonorte@sindipostos-ce.com.br"
+                            Email: subsederegiaonorte@sindipostos-ce.com.br"
                             label='Sobral'
                             path='/services'
                         />
-                    </ul>
-                    <ul className="cards__items">
                         <CardItem 
-                            src="images/img-Fortal.jpg"
-                            text="Avenida Engenheiro Santana Junior, 
-                            3000, sala: 605 CEP: 60192-200 
-                            Cel.: (85) 9 9137-7964 Telefone: (85) 3244.1147 
-                            Email: sindipostos@sindipostos-ce.com.br"
-                            label='Fortaleza'
+                            src="images/img-JuazeiroNO.jpg"
+                            text="Av. Ailton Gomes, 4131 – Sala 207 – Lagoa Seca
+                            – Cep: 63020-000,
+                            Fone: (88) 3571.2208
+                            Email: subsederegiaodocariri@sindipostos-ce.com.br"
+                            label='Juazeiro do Norte'
                             path='/services'
                         />
                     </ul>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,46 @@
 import React from 'react'
 
+import { Button } from './Button'
+import './Footer.css'
+
 function Footer() {
     return (
         <div className='footer-container'>
             <section className="footer-subscription">
                 <p className="footer-subscription-heading">
-                    Redes Sociais e Contatos
+                    Redes Sociais e Informações
                 </p>
                 <p className="footer-subscription-text">
-                    
+                    Inscreva-se para receber Informações atualizadas no seu e-mail
                 </p>
+                <div className="input-areas">
+                    <form>
+                        <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="Seu email"
+                        className="footer-input"
+                        />
+                         <Button buttonStyle='btn--outline'>Inscreva-se</Button>
+                    </form>
+                </div>
             </section>
+            <div className="footer-links">
+            <a 
+            className="social-instagram"
+            href="https://www.instagram.com/sindipostosce/?hl=pt"
+
+            >
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a 
+            className="social-facebook"
+            href="https://www.facebook.com/sindipostosce/"
+            
+            >
+                <i class="fab fa-facebook"></i>
+            </a>
+            </div>
         </div>
     )
 }
