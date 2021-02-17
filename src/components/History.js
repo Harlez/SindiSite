74b8bbad1f3bb7ce00015dfc3eb76a329a43e749
Harlez <62451203+Harlez@users.noreply.github.com>
2021-02-17 16:48:01 -0300

    Add files via upload

---
+++ src/components/History.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import './History.css'
+
+function History() {
+    return (
+        <div className="history-container">
+            <div className="history-title">
+                <h1>
+                Nossa<br/>
+                História
+                </h1>
+            </div>
+                <div className="history-text">
+                    <h3>
+                    Das conversas entre amigos que aconteciam na frente do posto de Nazário Gomes de Oliveira e que contavam com a participação de Hélio Chagas Leitão, Eloy Fontenele Saboya, Fenelon Moreira Cals, Joaquim Tavares de Luna e José Eleutério, entre outros, nascia em 1965 à idéia para criação da Associação do Comércio Varejista de Combustíveis Minerais de Fortaleza.
+
+                    A primeira diretoria da associação teve a seguinte composição: Presidente: Hélio Chagas Leitão; Vice-Presidente: Eloy Fontenele Saboya; Tesoureiro: João Alves Ferreira; Secretário: Fenelon Moreira Cals. Já nesta época, a associação contava com o quadro de 20 associados que se reuniam na sua primeira sede situada à Rua Dr. João Moreira, logo ao lado da Santa Casa de Misericórdia.
+
+                    Com o crescimento de dúvidas freqüentes, neste ramo tão peculiar que é a revenda de combustíveis, foi observada a necessidade de criar um sindicato que representasse esta classe no Estado do Ceará e oferecesse serviços específicos.
+
+                    Na data de 28 de junho de 1968 a associação transformou-se no Sindicato do Comércio Varejista de Combustíveis Minerais do Estado do Ceará, funcionando em uma sala no então majestoso edifício Palácio do Progresso, na região central da cidade de Fortaleza. Sua primeira diretoria teve a seguinte formação: Presidente: Eloy Fontenele Saboya, Vice-Presidente: Fenelon Moreira Cals, Tesoureiro: João Alves Ferreira, Secretário: João Jeová Mendes.
+
+                    Em 24 de Novembro de 1988, o Sindicato inaugurou a sua sede própria, na Rua Visconde de Mauá, nº. 1.510 - Bairro Aldeota. Em 15 de maio de 2014, mudou-se para a sua nova sede, na Av. Engenheiro Santana Junior, nº 3000/6º andar – Bairro: Parque do Cocó, onde funciona até a data atual com a razão social Sindicato do Comércio Varejista de Derivados de Petróleo do Estado do Ceará – SINDIPOSTOS/CE.
+                    </h3>
+                </div>
+        </div>
+    )
+}
+
+export default History
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,12 +3,14 @@ import HeroSection from '../HeroSection'
 import Services from '../Services'
 import Cards from '../Cards'
 import Footer from '../Footer'
+import History from '../History'
 
 function Home () {
     return (
         <>
             <HeroSection/>
             <Services />
+            <History/>
             <Cards />
             <Footer />
         </>
